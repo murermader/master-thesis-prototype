7bfd87ab1120b658c292a08ef597e1b08b4d1d78
Rafael Biehler <mail@rafaelbiehler.com>
2024-11-01 16:07:39 +0100

    change stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'master-thesis-prototype';
+  protected readonly console = console;
+  items = [
+    {title: 'Explore the Docs', link: 'https://angular.dev'},
+    {title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials'},
+    {title: 'CLI Docs', link: 'https://angular.dev/tools/cli'},
+    {title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service'},
+    {title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools', onClick: () => console.log("Hello World!")},
+  ]
+
+  // Type guard to check if an item has an onClick property
+  hasOnClick(item: { link: string; title: string; onClick?: () => void }): item is { link: string; title: string; onClick: () => void } {
+    return typeof item.onClick === 'function';
+  }
+
 }
+
