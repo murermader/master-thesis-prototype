5f0d9a4af7ea9e63ef18c36859b89ef9a667215b
Rafael Biehler <mail@rafaelbiehler.com>
2024-11-06 15:11:28 +0100

    Dot not call function with injector directly from DOM inside @for

--- src/app/models/MapLayer.model.ts
+++ src/app/models/MapLayer.model.ts
@@ -1,16 +1,21 @@
 import { Visualization } from './visualization/Visualization.model';
 import {PolyphenyResult} from "./PolyphenyResult.model";
+import {Injector} from "@angular/core";
 
 export class MapLayer {
     name: string;
     data: PolyphenyResult[];
     visualization: Visualization;
     is_active: boolean;
+    injector: Injector;
 
     constructor(name: string, data: PolyphenyResult[], visualization: Visualization) {
         this.name = name
         this.data = data;
         this.visualization = visualization;
         this.is_active = true;
+        this.injector = Injector.create({
+            providers: [{ provide: 'config', useValue: visualization }],
+        });
     }
 }
