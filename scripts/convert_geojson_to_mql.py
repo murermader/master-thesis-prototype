7293af33653a2cc214f8e74c9256462d1441c61c
Rafael Biehler <mail@rafaelbiehler.com>
2024-12-18 02:05:09 +0100

    Add bern_temp file

--- scripts/convert_geojson_to_mql.py
+++ scripts/convert_geojson_to_mql.py
@@ -1,4 +1,5 @@
 import json
+import re
 import uuid
 
 
@@ -23,22 +24,28 @@ item_template = """
 }"""
 
 items = []
+allowed_characters = re.compile(r'[^a-zA-Zäöüß\-. ]')
 
 for idx, d in enumerate(data["features"]):
-    lat = str(d['geometry']['coordinates'][1])
-    lon = str(d['geometry']['coordinates'][0])
-    plac = d['properties']['PLAC']
+    lat = d['geometry']['coordinates'][0]
+    lon = d['geometry']['coordinates'][1]
+    plac = allowed_characters.sub('', d['properties']['PLAC'])
     d_id = str(uuid.uuid4())
-    item = (
-        item_template.replace("<LAT>", lat)
-        .replace("<LON>", lon)
-        .replace("<PLAC>", plac)
-        .replace("<ID>", d_id)
-    )
-    items.append(item)
 
-    if idx == 100:
-        break
+    # Create the object directly
+    item = {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [lat, lon]
+        },
+        "place": plac,
+        "id": d_id,
+    }
+    # Add the serialized JSON string to the items list
+    items.append(json.dumps(item))
+
+    # if idx == 75:
+    #     break
 
 mql = f"db.individualCoords.insertMany([{','.join(items)}])"
 with open("individualCoords.mql", mode='w', encoding="utf-8") as f:
