d43ac2fcc827ac52ea26d9e9c2b7cb98c1a8693b
Rafael Biehler <mail@rafaelbiehler.com>
2025-01-19 13:44:19 +0100

    proficiency_to_sus.py

--- survey/proficiency_to_sus.py
+++ survey/proficiency_to_sus.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("data_prof_sus.csv")
 df = df.drop(columns=['What is your role? '])
-print(df.head())
 
-correlation = df.corr()['SUS Score']
-correlation = correlation.drop('SUS Score')
-correlation = correlation.sort_values(ascending=False)
-print(correlation)
-# The strongest positive correlation is with GIS experience (0.356), while years of programming experience (-0.144) has a slight negative correlation.
+for method in {'pearson', 'kendall', 'spearman'}:
+    correlation = df.corr(method=method)['SUS Score']
+    correlation = correlation.drop('SUS Score')
+    correlation = correlation.sort_values(ascending=False)
+    print(method)
+    print(correlation)
